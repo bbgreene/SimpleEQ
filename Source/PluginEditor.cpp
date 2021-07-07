dfaaf439dcc74c4ff4fbd294c0fa57af08afb4a1
bbgreene <brian.greene.music@gmail.com>
2021-07-07 12:51:57 +0200

    Part 13 Response Curve Grid - added Analyser decibel marks
    cleaned up code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -231,8 +231,7 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
     
     g.drawImage(background, getLocalBounds().toFloat());
 
-//    auto responseArea = getLocalBounds();
-    auto responseArea = getAnalysisArea();//getRenderArea();
+    auto responseArea = getAnalysisArea();
     
     auto w = responseArea.getWidth();
     
@@ -328,12 +327,8 @@ void ResponseCurveComponent::resized()
     }
     
     g.setColour(Colours::dimgrey);
-//    for( auto f : freqs )
     for( auto x : xs )
     {
-//        auto normX = mapFromLog10(f, 20.f, 20000.f);
-        
-//        g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
         g.drawVerticalLine(x, top, bottom);
     }
     
@@ -345,12 +340,9 @@ void ResponseCurveComponent::resized()
     for( auto gDb : gain )
     {
         auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
-//        g.drawHorizontalLine(y, 0, getWidth());
         g.setColour(gDb == 0.f ? Colour(227u, 85u, 43u) : Colours::darkgrey );   // 1st set of colours is centre line
         g.drawHorizontalLine(y, left, right);
     }
-    
-//    g.drawRect(getAnalysisArea());
     
     g.setColour(Colours::lightgrey);
     const int fontHeight = 10;
@@ -401,8 +393,19 @@ void ResponseCurveComponent::resized()
         r.setCentre(r.getCentreX(), y);
         
         g.setColour(gDb == 0.f ? Colour(227u, 85u, 43u) : Colours::lightgrey );
-        
+    
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+        str.clear();
+        str << (gDb - 24.f);
+        
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+        
     }
 }
  
@@ -410,9 +413,6 @@ juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
 {
     auto bounds = getLocalBounds();
     
-//    bounds.reduce(10, //JUCE_LIVE_CONSTANT(5),
-//                  8 //JUCE_LIVE_CONSTANT(5));
-//                  );
     bounds.removeFromTop(12);
     bounds.removeFromBottom(2);
     bounds.removeFromLeft(20);
